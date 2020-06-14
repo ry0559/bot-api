619679b5e3514fc50d0963802b0eaf671199ef3b
ry0559 <r08014019606@gmail.com>
2020-06-14 21:29:05 +0900

    cors編集

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:3000','https://ry0559.github.io/chat-bot/'
 
     resource '*',
       headers: :any,
